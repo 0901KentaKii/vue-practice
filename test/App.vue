4ac204a53d855f5462a8108766b5db30a8f04deb
0901Kentakii <0ar3295725m2k3y@ezweb.ne.jp>
2024-12-28 01:26:54 +0900

    commit test

---
+++ test/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <h1>{{ message }}</h1>
+    <button @click="changeMessage">メッセージを変更</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      message: 'こんにちは Vue!'
+    }
+  },
+  methods: {
+    changeMessage() {
+      this.message = 'メッセージが変更されました!'
+    }
+  }
+}
+</script>
+
+<style scoped>
+h1 {
+  color: #42b983;
+}
+button {
+  padding: 10px 20px;
+  background-color: #42b983;
+  color: white;
+  border: none;
+  border-radius: 4px;
+  cursor: pointer;
+}
+button:hover {
+  background-color: #3aa876;
+}
+</style>
